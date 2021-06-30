d26b1a0b95854190e6a07fc37873c9f15df81df6
Susanne85 <xkqk2010@gmail.com>
2021-06-30 18:41:09 +0800

    Updated form processing and views

--- controllers/form-routes.js
+++ controllers/form-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/form', async (request, response) => {
-    console.log('Home Routes - form', request.url);
+    console.log('Form - form', request.url);
     response.render('form');
   });
   
